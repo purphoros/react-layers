8a42d1fc33a4196d061e451824fe269b23b1b009
purphoros <4297636+purphoros@users.noreply.github.com>
2022-04-19 12:54:13 +1000

    Update: 19/04/2022 12:54:13

--- src/components/ActiveLayers.tsx
+++ src/components/ActiveLayers.tsx
@@ -43,13 +43,14 @@ const ActiveLayers = (props: Props) => {
   return <React.Fragment>
     {activeLayers.map(({
       layerUuid,
+      updatedOn,
       children,
       orientation = "modal"
     }: Layer) => {
       const custom = (orientation && alignments[orientation]) ? alignments[orientation] : alignments.default;
       return <div key={layerUuid} style={{ ...custom, display: "flex", flexDirection: "column", position: "absolute", top: "0px", right: "0px", bottom: "0px", left: "0px", backgroundColor: "transparent" }}>
         <div style={{ position: "absolute", top: "0px", right: "0px", bottom: "0px", left: "0px", backgroundColor: "black", opacity: "0.5" }} onClick={() => layerUuid && closeLayerByUuid(layerUuid)} />
-        {cloneElement(children, { ...children.props, style: { ...children.props.style, backgroundColor: "#FF0000", position: "relative" }, layerUuid: layerUuid })}
+        {cloneElement(children, { ...children.props, style: { ...children.props.style, backgroundColor: "#FF0000", position: "relative" }, layerUuid, updatedOn })}
       </div>
     })}
   </React.Fragment>
