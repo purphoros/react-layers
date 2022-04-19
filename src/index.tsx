38c6b34b1c86961c4fe435e57a032e8a01bb1292
purphoros <4297636+purphoros@users.noreply.github.com>
2022-04-19 12:38:41 +1000

    Update: 19/04/2022 12:38:41

--- src/index.tsx
+++ src/index.tsx
@@ -85,7 +85,7 @@ const LayerContextProvider = ({ children }: { children: ReactNode }) => {
   );
 
   const activeLayers = layers
-    .sort((a, b) => (a.updatedOn ?? 0) < (b.updatedOn ?? 0) ? -1 : 1)
+    .sort((a: any, b: any) => a.updatedOn > b.updatedOn ? 1 : a === b ? 0 : -1)
     .filter((d: any) => !!d.status);
 
   return <LayerContext.Provider value={contextValue}>
