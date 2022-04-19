ff58822e5ca8ec88fb6369e613b275a890bc5fbd
purphoros <4297636+purphoros@users.noreply.github.com>
2022-04-19 15:18:17 +1000

    Update: 19/04/2022 15:18:17

--- build/components/ActiveLayers.d.ts
+++
@@ -1,7 +0,0 @@
-/// <reference types="react" />
-interface Props {
-    layers: any;
-    closeLayerByUuid: (layerUuid: string) => void;
-}
-declare const ActiveLayers: (props: Props) => JSX.Element;
-export default ActiveLayers;
